c8e3cac0706f1201432bd355ed72c957847c2b80
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-06-03 17:41:57 +0200

    feat: added categories to server router

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,44 @@
+// Some data to make the trick
+
+const categories = [
+  {
+    id: 1,
+    name: "Comédie",
+  },
+  {
+    id: 2,
+    name: "Science-Fiction",
+  },
+];
+
+// Declare the actions
+
+import type { RequestHandler } from "express";
+
+const browse: RequestHandler = (req, res) => {
+  if (req.query.q != null) {
+    const filteredCategories = categories.filter((category) =>
+      category.name.includes(req.query.q as string),
+    );
+
+    res.json(filteredCategories);
+  } else {
+    res.json(categories);
+  }
+};
+
+const read: RequestHandler = (req, res) => {
+  const parseId = Number.parseInt(req.params.id);
+
+  const category = categories.find((cat) => cat.id === parseId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// Export them to import them somewhere else
+
+export default { browse, read };
--- server/src/router.ts
+++ server/src/router.ts
@@ -13,17 +13,25 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Define program-related routes
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
 /* ************************************************************************* */
 
 // Declaration of a "Welcome" route
+
 import sayActions from "./modules/say/sayActions";
 
 router.get("/", sayActions.sayWelcome);
 
 /* ************************************************************************* */
 
-import programActions from "./modules/program/programActions";
+import categoryActions from "./modules/category/categoryActions";
 
-router.get("/api/programs", programActions.browse);
+router.get("/api/categories", categoryActions.browse);
+router.get("pai/categories/:id", categoryActions.read);
 
 export default router;
